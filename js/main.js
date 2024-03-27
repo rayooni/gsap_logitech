03401b2f02b3729d549cf7a3f19b20a0a7e004c4
rayooni <xpelxpel123@naver.com>
2024-03-27 17:43:25 +0900

    menu...

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
-  //==== html document 요소가 모두 로드된 후 실행될 코드 : gsap가 자체적으로 생성하는 요소를 제어하기 위한 코드
+  //=== html document 요소가 모두 로드된후 실행될 코드 : gsap가 자체적으로 생성하는 요소를 제어하기 위한 코드
 
   // 1. 커서 효과
   const cursor = $('#cursor');
+
   $(document).on('mousemove', function (e) {
     gsap.to(cursor, {
       left: e.pageX - cursor.width() * 0.5,
@@ -21,11 +22,23 @@ window.addEventListener('DOMContentLoaded', function () {
       gsap.to('.dot', 0.5, { scale: 0.15, background: '#4274ff' });
     });
 
-  $('#header')
+  $('#header, #menu .menu_header')
     .mouseenter(function () {
       $(this).addClass('on');
     })
     .mouseleave(function () {
       $(this).removeClass('on');
     });
+
+  $('#header .ham').click(function () {
+    $('#menu').fadeToggle().toggleClass('on');
+  });
+
+  $('#menu .menu_header .ham').click(function () {
+    $('#menu').fadeToggle().toggleClass('on');
+  });
+
+  // side image move half left and right
+  const sideImgW = $('.star div:nth-child(1)').width();
+  $('.side').css('width', `calc(100% + ${sideImgW}px)`);
 });
